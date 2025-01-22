9be5788a70f6643802228cefeaf8a6cedfcd3fca
Vishavs15 <vishvassbapodara@gmail.com>
2025-01-22 22:54:41 +0530

    Hero Section Done

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,18 +2,24 @@ import Button from "@/components/Button";
 import designexample1img from "@/assets/images/design-example-1.png";
 import designexample2img from "@/assets/images/design-example-2.png";
 import Image from "next/image";
+import Pointer from '@/components/Pointer';
 
 export default function Hero() {
     return (
-        <section className="py-24">
+        <section className="py-24 overflow-x-clip">
             <div className="container relative">
-                <div className="absolute -left-32 top-16">
+                <div className="absolute -left-32 top-16 hidden lg:block">
                     <Image src={designexample1img} alt="designexample1" />
                 </div>
-                <div className="absolute -right-64 -top-16">
+                <div className="absolute -right-64 -top-16 hidden lg:block">
                     <Image src={designexample2img} alt="designexample2" />
                 </div>
-
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name='Andres'/>
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name='Bryan' color="red"/>
+                </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         🎃 $7.5M seed round raised
